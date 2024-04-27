b3a9e1e64c9166d26cb765f8f9900224dbc73b4c
dot96gal <dot96gal@gmail.com>
2024-04-27 13:23:02 +0900

    reafactor icon_button

--- components/icon_button_templ.go
+++ components/icon_button_templ.go
@@ -11,18 +11,19 @@ import "io"
 import "bytes"
 import "strings"
 
-func iconButtonStyle() templ.CSSClass {
+func iconButtonButtonStyle() templ.CSSClass {
 	var templ_7745c5c3_CSSBuilder strings.Builder
 	templ_7745c5c3_CSSBuilder.WriteString(`display:block;`)
 	templ_7745c5c3_CSSBuilder.WriteString(`width:24px;`)
 	templ_7745c5c3_CSSBuilder.WriteString(`height:24px;`)
-	templ_7745c5c3_CSSID := templ.CSSID(`iconButtonStyle`, templ_7745c5c3_CSSBuilder.String())
+	templ_7745c5c3_CSSID := templ.CSSID(`iconButtonButtonStyle`, templ_7745c5c3_CSSBuilder.String())
 	return templ.ComponentCSSClass{
 		ID:    templ_7745c5c3_CSSID,
 		Class: templ.SafeCSS(`.` + templ_7745c5c3_CSSID + `{` + templ_7745c5c3_CSSBuilder.String() + `}`),
 	}
 }
 
+// [TODO] ボタン押下時のアクションを追加する
 func IconButton(icon templ.Component) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
@@ -36,7 +37,7 @@ func IconButton(icon templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		var templ_7745c5c3_Var2 = []any{iconButtonStyle()}
+		var templ_7745c5c3_Var2 = []any{iconButtonButtonStyle()}
 		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
